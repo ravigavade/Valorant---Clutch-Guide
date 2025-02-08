d24b9342d8dd745a4f6d0e5216f0d35c7a757615
Ravi Gavade <csaim@Ravis-MacBook-Pro.local>
2025-02-07 19:04:23 -0500

    blue added agents

--- app/src/main/java/com/csaim/valorant_clutchguide/AgentScreen.kt
+++ app/src/main/java/com/csaim/valorant_clutchguide/AgentScreen.kt
@@ -5,9 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.csaim.valorant_clutchguide.ui.theme.ValorantClutchGuideTheme
 
 class AgentScreen : ComponentActivity() {
@@ -16,12 +30,41 @@ class AgentScreen : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ValorantClutchGuideTheme {
+                val agents= listOf("Viper","Brimstone","Cypher","Reina","Iso", "Clove")
+
+
                 Column {
-                    Text(text = "Agent Selection Screen")
-                    Button(onClick = {
-                        startActivity(Intent(this@AgentScreen,SideScreen::class.java))
-                    }) {
-                        Text(text = "got to Team Screen")
+
+                    //replancement of recycler view
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(16.dp)
+                    ) {
+                        items(agents){ agentName->//maps is the list of maps
+                            //card view for better ui
+                            Card(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillParentMaxWidth()
+                                    .height(50.dp),
+                                shape = RoundedCornerShape(12.dp),
+                                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                                onClick = { startActivity(Intent(this@AgentScreen,SideScreen::class.java)) }
+
+                            ) {
+                                //box to center the text
+                                Box(
+                                    modifier = Modifier.fillMaxSize(), // Make the Box fill the whole Card
+                                    contentAlignment = Alignment.Center // Center the content inside the Box
+                                ) {
+                                    Text(
+                                        text = agentName,
+                                        fontSize = 18.sp,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
             }
