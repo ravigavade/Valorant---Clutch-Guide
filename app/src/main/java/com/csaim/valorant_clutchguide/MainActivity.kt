f8d121e83f763adf07ac89560ea7114f7bf19b1c
Ravi Gavade <gavaderavi4@gmail.com>
2025-02-15 18:35:36 -0500

    bottom bar

--- app/src/main/java/com/csaim/valorant_clutchguide/MainActivity.kt
+++ app/src/main/java/com/csaim/valorant_clutchguide/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -22,19 +21,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -45,7 +35,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.csaim.valorant_clutchguide.ui.theme.DarkBlueGray
 import com.csaim.valorant_clutchguide.ui.theme.DarkBlueGray2
+import com.csaim.valorant_clutchguide.ui.theme.DarkRed
 import com.csaim.valorant_clutchguide.ui.theme.MuchDarkBlueGray
+import com.csaim.valorant_clutchguide.ui.theme.RedPrimary
 import com.csaim.valorant_clutchguide.ui.theme.ValorantClutchGuideTheme
 import com.csaim.valorant_clutchguide.ui.theme.valo
 
@@ -291,19 +283,16 @@ fun home_screen(modifier: Modifier = Modifier) {
                         }
 
                     }
+
+
+                    //the bottom bar hahahahhaha
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(80.dp)
                             .background(MuchDarkBlueGray)
                     ){
-                        Text(
-                            text = "Bottom Bar hahahah",
-                            color = Color.White,
-                            fontFamily = valo,
-                            fontSize = 30.sp,
-                            textAlign = TextAlign.Center,
-                        )
+                        DropDownDemo()
                     }
 
 
@@ -439,4 +428,213 @@ fun NonActiveMaps(navController: NavController) {
     }
 }
 
+@Composable
+fun DropDownDemo() {
 
+    val isMapDropDownExpanded = remember { mutableStateOf(false) }
+    val isAgentDropDownExpanded = remember { mutableStateOf(false) }
+    val isSideExpanded = remember { mutableStateOf(false) }
+    val isSiteExpanded = remember { mutableStateOf(false) }
+
+    val itemPosition = remember { mutableStateOf(0) }
+    val itemPosition1 = remember { mutableStateOf(0) }
+    val itemPosition3 = remember { mutableStateOf(0) }
+    val itemPosition4 = remember { mutableStateOf(0) }
+
+    val maps = listOf("Abyss", "Pearl", "Fracture", "Split", "Haven", "Lotus", "Bind")
+    val agents = listOf("Brimstone", "Viper", "Omen", "Phoenix", "Jett", "Sage", "Breach", "Cypher", "Sova", "Raze", "Killjoy", "Reyna", "Skye", "Yoru", "Astra", "KAY/O")
+    val sides = listOf("Attack", "Defense")
+    val sites = listOf("Site A", "Side B")
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 10.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+
+    ) {
+
+        // Maps Dropdown
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable {
+                isMapDropDownExpanded.value = !isMapDropDownExpanded.value
+            }
+        ) {
+            Text(
+                text = maps[itemPosition.value],
+                fontFamily = valo,
+                color = Color.White
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+                contentDescription = "Map Dropdown Icon",
+                tint = RedPrimary
+            )
+        }
+
+        DropdownMenu(
+            modifier = Modifier
+                .height(300.dp)
+                .background(MuchDarkBlueGray),
+            expanded = isMapDropDownExpanded.value,
+            onDismissRequest = { isMapDropDownExpanded.value = false },
+        ) {
+            maps.forEachIndexed { index, agentname ->
+                DropdownMenuItem(text = {
+                    Text(
+                        text = agentname,
+                        fontFamily = valo,
+                        color = Color.White
+                    )
+//                    Divider()
+                },
+                    onClick = {
+                        isMapDropDownExpanded.value = false
+                        itemPosition.value = index
+                    })
+            }
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        // Agents Dropdown
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+                isAgentDropDownExpanded.value = !isAgentDropDownExpanded.value
+            }
+        ) {
+            Text(
+                text = agents[itemPosition1.value],
+                fontFamily = valo,
+                color = Color.White
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+                contentDescription = "Agent Dropdown Icon",
+                tint = RedPrimary
+            )
+        }
+
+        DropdownMenu(
+            modifier = Modifier
+                .height(300.dp)
+                .background(MuchDarkBlueGray),
+            expanded = isAgentDropDownExpanded.value,
+            onDismissRequest = { isAgentDropDownExpanded.value = false }
+        ) {
+            agents.forEachIndexed { index, agentname ->
+                DropdownMenuItem(
+//                    modifier = Modifier.background(MuchDarkBlueGray),
+                    text = {
+                    Text(
+                        text = agentname,
+                        fontFamily = valo,
+                        color = Color.White
+                    )
+                },
+                    onClick = {
+                        isAgentDropDownExpanded.value = false
+                        itemPosition1.value = index
+                    })
+            }
+        }
+
+
+        //side dropdown
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+                isSideExpanded.value = !isSideExpanded.value
+            }
+        ) {
+            Text(
+                text = sides[itemPosition3.value],
+                fontFamily = valo,
+                color = Color.White
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+                contentDescription = "Side Dropdown Icon",
+                tint = RedPrimary
+            )
+        }
+
+        DropdownMenu(
+            modifier = Modifier
+                .height(120.dp)
+                .background(MuchDarkBlueGray),
+            expanded = isSideExpanded.value,
+            onDismissRequest = { isSideExpanded.value = false }
+        ) {
+            sides.forEachIndexed { index, sidename ->
+                DropdownMenuItem(
+//                    modifier = Modifier.background(MuchDarkBlueGray),
+                    text = {
+                        Text(
+                            text = sidename,
+                            fontFamily = valo,
+                            color = Color.White
+                        )
+                    },
+                    onClick = {
+                        isSideExpanded.value = false
+                        itemPosition3.value = index
+                    })
+            }
+        }
+
+
+
+        //site dropdown
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.clickable {
+                isSiteExpanded.value = !isSiteExpanded.value
+            }
+        ) {
+            Text(
+                text = sites[itemPosition4.value],
+                fontFamily = valo,
+                color = Color.White
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+                contentDescription = "Site Dropdown Icon",
+                tint = RedPrimary // Set the tint color to white
+            )
+        }
+
+        DropdownMenu(
+            modifier = Modifier
+                .height(120.dp)
+                .background(MuchDarkBlueGray),
+            expanded = isSiteExpanded.value,
+            onDismissRequest = { isSiteExpanded.value = false }
+        ) {
+            sites.forEachIndexed { index, sitename ->
+                DropdownMenuItem(
+//                    modifier = Modifier.background(MuchDarkBlueGray),
+                    text = {
+                        Text(
+                            text = sitename,
+                            fontFamily = valo,
+                            color = Color.White
+                        )
+                    },
+                    onClick = {
+                        isSiteExpanded.value = false
+                        itemPosition4.value = index
+                    })
+            }
+        }
+
+
+    }
+}
--- app/src/main/java/com/csaim/valorant_clutchguide/ui/theme/Color.kt
+++ app/src/main/java/com/csaim/valorant_clutchguide/ui/theme/Color.kt
@@ -6,7 +6,7 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 val dardred = Color(0xFFBA3A46)
-val lightred = Color(0xFFFF4654)
+val lightred = Color(0xFF1E2022)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
